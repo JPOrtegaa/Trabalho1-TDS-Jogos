53af95676ec81047db5fe05b6d5d9024a68e0e9a
JPOrtegaa <joaopedro.ortega17@gmail.com>
2024-04-04 11:48:17 -0300

    added event for clicked jogos in lista jogos

--- js/index.js
+++ js/index.js
@@ -9,23 +9,20 @@ function Game(nome, dev, data, categorias, descricao, imagens){
     this.imagens = imagens;
 }
 
-// sessionStorage.setItem('gameList', 'salve'); 
-
 let gameList = sessionStorage.getItem('gameList') || [];
-gameList
-// console.log(conditionalList)
 
 if(gameList.length === 0){
     console.log("nao criada!!");
-    const game1 = new Game('Dead by Daylight', 'dev', '2024-10-05', ['RPG', 'Outros'], 'dbd', ['imagens/dbdlogo.jpg']);
+    const game1 = new Game('Dead by Daylight', 'dev', '2024-10-05', ['RPG', 'Outros'], 'dbd', 'imagens/dbdlogo.jpg');
     gameList.push(game1);
     sessionStorage.setItem('gameList', JSON.stringify(gameList));
     console.log(gameList);
+    printListaJogos(gameList);
 }
 else{
-    // gameList = JSON.parse(conditionalList);
     gameList = JSON.parse(gameList);
     console.log(gameList);
+    printListaJogos(gameList);
 }
 
 
@@ -41,56 +38,39 @@ addGameButton.addEventListener("click", function (event) {
 
 });
 
+function printListaJogos(gameList){
 
-// // Evento para adicionar um novo jogo
-// const addGameButton = document.getElementById('addjogo');
+    gameList.forEach(game => {
+        // Create a new game div
+        const newGameDiv = document.createElement("div");
+        newGameDiv.classList.add("jogo");
+    
+        // Example: Add an image and text to the new game div
+        const newGameImage = document.createElement("img");
+        newGameImage.classList.add("capa");
+        newGameImage.src = game.imagens; // Add the source of your game image here
+        newGameDiv.appendChild(newGameImage);
+        
+        const newGameTitle = document.createTextNode(game.nomeJogo); // Change the text as needed
+        newGameDiv.appendChild(newGameTitle);
+        
+        // Append the new game div to the game list
+        const gameList = document.querySelector(".jogos");
+        gameList.appendChild(newGameDiv);
+    });
 
-// addGameButton.addEventListener("click", function (event) {
-//     event.preventDefault();
 
-//     // const game1 = new Game(
-//     //   "Dead by Daylight",
-//     //   "dev",
-//     //   "2024-10-05",
-//     //   ["RPG", "Outros"],
-//     //   "dbd",
-//     //   ["imagens/dbdlogo.jpg"]
-//     // );
-
-//     // const gameList = [];
-//     // gameList.push(game1);
-
-//     // console.log(localStorage.getItem('nome'));
-//     // console.log(gameList);
-
-//     // // Create a new game div
-//     // const newGameDiv = document.createElement("div");
-//     // newGameDiv.classList.add("jogo");
-
-//     // // Example: Add an image and text to the new game div
-//     // const newGameImage = document.createElement("img");
-//     // newGameImage.classList.add("capa");
-//     // newGameImage.src = "imagens/dbdlogo.jpg"; // Add the source of your game image here
-//     // newGameDiv.appendChild(newGameImage);
-
-//     // const newGameTitle = document.createTextNode("Dead by Daylight"); // Change the text as needed
-//     // newGameDiv.appendChild(newGameTitle);
-
-//     // // Append the new game div to the game list
-//     // const gameList = document.querySelector(".jogos");
-//     // gameList.appendChild(newGameDiv);
-// });
+}
 
 
 // Evento para pegar a info sobre o jogo clicado
-const gameDiv = document.querySelector('.jogo');
+const gameDivs = document.querySelectorAll('.jogo');
+console.log(gameDivs);
 
-gameDiv.addEventListener("click", function(event){
-
-    event.preventDefault();
-
-    const gamePage = 'jogo.html';
-
-    window.location.href = gamePage;
-
+gameDivs.forEach(gameDiv => {
+    gameDiv.addEventListener("click", function (event) {
+      event.preventDefault();
+      const gamePage = "jogo.html";
+      window.location.href = gamePage;
+    });    
 });
